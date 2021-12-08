3b8dc7650b61734802a32ea7887064abff06631d
NWilde4 <wilde.norman@yahoo.com>
2021-12-08 22:15:29 +0100

    correct route

--- index.js
+++ index.js
@@ -418,7 +418,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   const app = express()
   app.use(express.static('client/build'))
   app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile('client/build/index.html')
   })
   const httpServer = http.createServer(app)
   const server = new ApolloServer({
