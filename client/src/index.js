471dc4617f229d0ad3f9fca44af0aac7122ddfb3
NWilde4 <wilde.norman@yahoo.com>
2021-12-08 14:44:08 +0100

    add env port

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,9 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql'})
+const uri = process.env.PORT || "http://localhost:4000"
+
+const httpLink = new HttpLink({ uri: `${uri}/graphql`})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
